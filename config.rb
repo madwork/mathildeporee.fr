f58b9d3cee8895d743743a99554453ce1d9741be
Vincent Durand <vincent.durand@madwork.org>
2014-04-12 20:40:24 +0200

    Minify CSS and JS at build

--- config.rb
+++ config.rb
@@ -53,13 +53,17 @@ set :css_dir,     'stylesheets'
 set :js_dir,      'javascripts'
 set :images_dir,  'images'
 
+ready do
+  sprockets.append_path File.join root, 'components'
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
@@ -69,8 +73,4 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-end
-
-ready do
-  sprockets.append_path File.join root, 'components'
 end
