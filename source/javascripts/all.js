28087f0eb2527f721448111c3d0a5be3eff07e2b
Vincent Durand <vincent.durand@madwork.org>
2014-04-14 11:39:40 +0200

    Add spinner gallery loading

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,24 @@
 //= require masonry/dist/masonry.pkgd
 //= require imagesloaded/imagesloaded
 //= require colorbox/jquery.colorbox
+
+var gallery, msnry, imgLoad, colorbox_options;
+
+gallery = function(container, options) {
+  imgLoad = imagesLoaded(container);
+  imgLoad.on('done', function() {
+    document.querySelector('#spinner').style.display = 'none';
+    container.style.display = 'block';
+    msnry = new Masonry( container, options );
+  });
+}
+
+colorbox_options = {
+  rel:'gallery',
+  transition:"none",
+  height:"75%",
+  closeButton: false,
+  previous: "",
+  next: "",
+  current: ""
+};
